fd0b598a21f4e9b123781dd7f1f48adf8d86df73
alexspot <alex.kycherenko@gmail.com>
2013-08-13 12:50:18 +0300

    created 1st test & configured IDE to run tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ describe User do
   describe 'validations' do
     context 'when correct data given' do
       it 'should be valid' do
-        post = Post.new(title: 'Hello wordl', content: 'Lorem ipsum')
-        post.valid?.should be_true
+        user = User.new(name: 'lorem', email: 'lorem@ipsim.com', password: '12345678', password_confirmation: '12345678')
+        user.valid?.should be_true
       end
     end
   end
