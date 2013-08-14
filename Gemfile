4dad8b49d5c1d1496944ab7bd450f89c5720d70d
alexspot <alex.kycherenko@gmail.com>
2013-08-14 19:29:52 +0300

    Timecop added

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ group :development, :test do
   gem 'rspec-rails', '~> 2.0'
 end
 
+group :test do
+  gem 'timecop'
+end
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,67 @@
 require 'spec_helper'
 
+  def print_am_pm
+    if Time.now.hour >= 12
+      'PM'
+    else
+      'AM'
+    end
+  end
+
+describe '#print_am_pm' do
+  subject { print_am_pm }
+  after { Timecop.return }
+  context 'when current hour == 0' do
+    before { Timecop.freeze(Time.local(2008,9,9,0,0,0)) }
+    it { expect(subject).to eq('AM') }
+  end
+
+  context 'when current hour 11:59:59' do
+    before { Timecop.freeze(Time.local(2008,9,9,11,59,59)) }
+    it { expect(subject).to eq('AM') }
+  end
+
+  context 'when PM now' do
+    it { expect(subject).to eq('PM') }
+  end
+
+end
+
 describe User do
   describe 'validations' do
     context 'when correct data given' do
       it 'should be valid' do
         user = User.new(name: 'lorem', email: 'lorem@ipsim.com', password: '12345678', password_confirmation: '12345678')
+        user.valid?.should be_true
+      end
+    end
+    context 'when no name given'  do
+      it 'should raise error'  do
+        user = User.new(email: 'lorem@ipsim.com', password: '12345678', password_confirmation: '12345678')
+        user.valid?.should be_false
+      end
+    end
+    context 'when no email given'  do
+      it 'should raise error'  do
+        user = User.new(name: 'lorem',password: '12345678', password_confirmation: '12345678')
+        user.valid?.should be_false
+      end
+    end
+    context 'when no password given' do
+      it 'should raise error' do
+        user = User.new(name: 'lorem', email: 'lorem@ipsim.com',)
+        user.valid?.should be_false
+      end
+    end
+    context 'when name is 51 char long' do
+      it 'should raise error' do
+        user = User.new(name: 'loremloremloremloremloremloremloremloremloremlorem1', email: 'lorem@ipsim.com', password: '12345678', password_confirmation: '12345678')
+        user.valid?.should be_false
+      end
+    end
+    context 'when name is 50 char long' do
+      it 'should raise error' do
+        user = User.new(name: 'loremloremloremloremloremloremloremloremloremlorem', email: 'lorem@ipsim.com', password: '12345678', password_confirmation: '12345678')
         user.valid?.should be_true
       end
     end
